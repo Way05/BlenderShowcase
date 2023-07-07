3b5a975c72a1280e3957888a6cb5d35b292e1f68
Way05 <wtan235@gmail.com>
2023-07-07 14:00:59 -0400

    manifest get works

--- handleRequest.js
+++ handleRequest.js
@@ -1,7 +1,9 @@
 var apiKey = "9c9246aeb1b94fb7b6af308a45f0bf18";
 
+var xurVendorHash = "2190858386";
+
 var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://www.bungie.net/platform/Destiny/Manifest/InventoryItem/1274330687/", true);
+xhr.open("GET", `https://www.bungie.net/Platform/Destiny2/Manifest`, true);
 xhr.setRequestHeader("X-API-Key", apiKey);
 
 xhr.onreadystatechange = function()
@@ -9,7 +11,11 @@ xhr.onreadystatechange = function()
     if(this.readyState === 4 && this.status === 200)
     {
         var json = JSON.parse(this.responseText);
-        console.log(json.Response.data.inventoryItem.itemName);
+        console.log(json.ErrorStatus);
+    }
+    else if(this.status !== 200)
+    {
+        console.log(`Manifest fetch error ${this.status}`)
     }
 }
 
