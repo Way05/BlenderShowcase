b68e0bd3277a907f1490002e07e2114c6686714e
Way05 <130930847+Way05@users.noreply.github.com>
2023-07-08 16:10:13 -0400

    nice slideshow indicator/deco

--- slideshow.js
+++ slideshow.js
@@ -1,5 +1,5 @@
 const images = document.getElementsByClassName("sh");
-console.log(images.length);
+const buttons = document.querySelectorAll("button");
 
 var slideIndex = 1;
 showDiv(slideIndex);
@@ -17,11 +17,29 @@ function showDiv(n) {
     if(n < 1) {slideIndex = images.length};
 
     for(var i = 0; i < images.length; i++) {
-        images[i].style.display = "none";
+        images[i].style.opacity = "0";
     }
 
+    setTimeout(function() {for(var i = 0; i < images.length; i++) {
+        images[i].style.display = "none";
+    }
     images[slideIndex - 1].style.display = "block";
+    }, 500);
+    images[slideIndex - 1].style.opacity = "1";
+
+    for(var i = 0; i < buttons.length; i++) {
+        buttons[i].style.width = "10%";
+        buttons[i].style.backgroundColor = "gray";
+    }
+    buttons[slideIndex - 1].style.width = "15%";
+    buttons[slideIndex - 1].style.backgroundColor = "white";
 
     console.log(images[slideIndex - 1]);
     console.log(slideIndex);
 }
+
+function carousel() {
+    setTimeout(carousel, 5000);
+    incrementDiv(1);
+}
+carousel();
