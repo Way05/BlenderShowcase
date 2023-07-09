6b6895098798cc3ae976e3542dedee8df460ab84
Way05 <130930847+Way05@users.noreply.github.com>
2023-07-09 10:55:21 -0400

    scratched fade slideshow

--- slideshow.js
+++ slideshow.js
@@ -16,20 +16,6 @@ function showDiv(n) {
     if(n > images.length) {slideIndex = 1};
     if(n < 1) {slideIndex = images.length};
 
-    for(var i = 0; i < images.length; i++) {
-        images[i].style.opacity = "0";
-    }
-
-    setTimeout(function() {
-    for(var i = 0; i < images.length; i++) {
-        images[i].style.display = "none";
-    }
-    images[slideIndex - 1].style.display = "block";
-    }, 300);
-    setTimeout(function() {
-        images[slideIndex - 1].style.opacity = "1";
-    }, 305);
-
     for(var i = 0; i < buttons.length; i++) {
         buttons[i].style.width = "10%";
         buttons[i].style.backgroundColor = "gray";
