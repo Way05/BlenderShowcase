fc851ef666b94bf7a2d54b40ce18af7b198da5cc
Way05 <130930847+Way05@users.noreply.github.com>
2023-07-09 20:36:01 -0400

    REDID BUTTON ANIMATION +IMAGEBLUR, CLEANED UP CSS

--- indexOnLoadAnimation.js
+++ indexOnLoadAnimation.js
@@ -11,3 +11,15 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll('.hidden');
 
 hiddenElements.forEach((el) => observer.observe(el));
+
+var image = document.getElementById("bg");
+var button = document.getElementById("button");
+
+button.addEventListener("mouseover", () => {
+    image.style.filter = "blur(5px)";
+    image.style.transform = "scale(1.01)";
+})
+button.addEventListener("mouseleave", () => {
+    image.style.filter = "none";
+    image.style.transform = "scale(1)";
+})
