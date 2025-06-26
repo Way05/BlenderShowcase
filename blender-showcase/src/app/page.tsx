60e4449cc3f44ca291d06a9623c01f02e64c5284
Way05 <130930847+Way05@users.noreply.github.com>
2025-06-25 21:14:52 -0400

    fixed layout resizing bug

--- blender-showcase/src/app/page.tsx
+++ blender-showcase/src/app/page.tsx
@@ -6,13 +6,13 @@ import { Canvas } from "@react-three/fiber";
 export default function Home() {
     return (
         <div className="fullscreen">
-            <div className="grid h-screen w-screen grid-cols-2">
+            <div className="grid h-screen grid-cols-2">
                 <div className="flex items-center justify-center bg-black">
                     <h1 className="m-auto flex aspect-square items-center rounded-2xl p-10 text-4xl text-white shadow-2xl shadow-white">
                         Hi, I&apos;m Way.<br></br>I also do 3D modeling.
                     </h1>
                 </div>
-                <div className="bg-black">
+                <div className="h-screen bg-black">
                     <Canvas className="h-full w-full">
                         <ModelScene></ModelScene>
                     </Canvas>
