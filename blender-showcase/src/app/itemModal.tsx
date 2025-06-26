b6e463e06b5ec7e35ccd1496d439d4b4ea348571
Way05 <130930847+Way05@users.noreply.github.com>
2025-06-25 21:15:03 -0400

    hr styling

--- blender-showcase/src/app/itemModal.tsx
+++ blender-showcase/src/app/itemModal.tsx
@@ -19,8 +19,9 @@ export default function ItemModal(props: ItemModalProps) {
                 onClick={(e) => e.stopPropagation()}
                 className={`bg-opacity-75 grid h-2/3 ${props.square ? "w-fit grid-cols-3" : "w-2/3 grid-cols-5"} rounded-2xl bg-black p-5 text-white shadow-lg transition ${props.isOpen ? "scale-100 opacity-100" : "scale-125 opacity-0"}`}
             >
-                <div>
-                    <p className="pt-5 pb-5 text-2xl">{props.title}</p>
+                <div className="pr-5">
+                    <p className="pt-5 text-2xl">{props.title}</p>
+                    <hr className="mt-5 mb-5 w-10"></hr>
                     <p>{props.description}</p>
                 </div>
                 <div
