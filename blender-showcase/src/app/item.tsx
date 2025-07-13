b3c87686dcc4d32fa7326670da10ab3eee4cb8c7
Way05 <130930847+Way05@users.noreply.github.com>
2025-07-12 21:32:48 -0400

    default thumbnail added for src errors

--- blender-showcase/src/app/item.tsx
+++ blender-showcase/src/app/item.tsx
@@ -12,6 +12,8 @@ type ItemProps = {
 };
 
 export default function Item(props: ItemProps) {
+    const [thumbnail, setThumbnail] = useState(props.images[0]);
+    const [objectFit, setObjectFit] = useState("cover");
     const [isOpen, setIsOpen] = useState(false);
     return (
         <div
@@ -22,11 +24,15 @@ export default function Item(props: ItemProps) {
         >
             <div className="relative h-9/10">
                 <Image
-                    src={props.images[0]}
+                    src={thumbnail}
                     alt={props.title}
                     fill
-                    objectFit="cover"
+                    objectFit={objectFit}
                     className="h-full cursor-pointer rounded-2xl bg-black shadow-md shadow-black transition hover:scale-105 hover:shadow-lg"
+                    onError={() => {
+                        setThumbnail("/file.svg");
+                        setObjectFit("");
+                    }}
                 ></Image>
             </div>
             <p className="h-1/10 w-full cursor-pointer bg-white p-1 pt-3 text-xl transition hover:underline">
